f76348c6d8555bced1ae663c449661ea0c804a2b
gamename <tennis.n.smith@gmail.com>
2018-07-13 09:31:04 -0500

    Pexpect example answers

---
+++ test_driven_lab_answer/pexpect_dos_calculator_test.py
@@ -0,0 +1,19 @@
+#
+# Instructions: Write a test to verify your dos addition function works
+#
+import unittest
+from my_pexpect import *
+
+
+class TestDosMath(unittest.TestCase):
+    def test_multiplication(self):
+        result = dos_multiply(3,3)
+        self.assertEqual(9, result)
+
+
+    def test_addition(self):
+        result = dos_add(3,3)
+        self.assertEqual(6, result)
+
+if __name__ == '__main__':
+    unittest.main()
